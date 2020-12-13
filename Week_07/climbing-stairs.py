70da7a74400d4368ede94a0cba3f2a3eeec44d4b
klin111 <klin111@qq.om>
2020-12-13 20:19:30 +0800

    add climbing-stairs.py

--- Week_07/climbing-stairs.py
+++ Week_07/climbing-stairs.py
@@ -1,2 +1,11 @@
 # 70. 爬楼梯
 
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n==1 or n==2: return n
+        a, b, temp = 1, 2, 0
+        for i in range(3,n+1):
+            temp = a + b
+            a = b
+            b = temp
+        return temp
