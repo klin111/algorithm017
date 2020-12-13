3df40d4733c3836335773e87f7c095fe71d3cbc4
klin111 <klin111@qq.om>
2020-12-13 20:31:27 +0800

    add number-of-1-bits.py

--- Week_08/number-of-1-bits.py
+++ Week_08/number-of-1-bits.py
@@ -1,1 +1,14 @@
 # 191. 位1的个数
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        return bin(n).count("1")
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res=0
+        while n:
+            res+=n&1
+            n>>=1
+        return res
+
