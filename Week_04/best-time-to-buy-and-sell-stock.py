db9f8f3b03dfa0f66c3075dad54bf0b80bdd7fdd
klin111 <klin111@qq.om>
2020-12-13 18:39:12 +0800

    add best-time-to-buy-and-sell-stock.py

---
+++ Week_04/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,15 @@
+class Solution(object):
+
+    def maxProfit(self, prices):
+        if not prices or len(prices) is 1:
+            return 0
+
+        profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+
+        return profit
+
+
