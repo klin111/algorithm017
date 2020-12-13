bb7365c6ececb0fe635cec7dc288997a543abf81
klin111 <klin111@qq.om>
2020-12-13 20:53:37 +0800

    add reverse-only-letters.py

--- Week_09/reverse-only-letters.py
+++ Week_09/reverse-only-letters.py
@@ -1,1 +1,13 @@
 # 917. 仅仅反转字母
+
+class Solution(object):
+    def reverseOnlyLetters(self, S):
+        letters = [c for c in S if c.isalpha()]
+        ans = []
+        for c in S:
+            if c.isalpha():
+                ans.append(letters.pop())
+            else:
+                ans.append(c)
+        return "".join(ans)
+
